9f24b289a14d1b47f47c3b89b49277b766b27272
Brandon Fulljames <bfullj@gmail.com>
2018-10-26 19:07:12 -0700

    Simple character movement

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WGJ68Truth
-Weekly Game Jam #68: Truth
+
+[Weekly Game Jam #68: Truth](http://weeklygamejam.com/)
+
+## Assets Used
+
+* [Character Pack: Free Sample by Supercyan](https://assetstore.unity.com/packages/3d/characters/humanoids/character-pack-free-sample-79870)
---
+++ WGJ68Truth/Assets/Follow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+    public GameObject target;
+
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (target != null)
+        {
+            transform.position = target.transform.position + offset;
+        }
+	}
+}
