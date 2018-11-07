08e8612977f4c172bc9a16c695cb8523aca86f7f
Brandon Fulljames <bfullj@gmail.com>
2018-11-07 11:14:47 -0800

    Better menu

---
+++ WGJ68Truth/Assets/MainMenuCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCamera : MonoBehaviour {
+
+    public float RotateSpeed = 15f;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0f, RotateSpeed * Time.deltaTime, 0f, Space.World);
+	}
+}
