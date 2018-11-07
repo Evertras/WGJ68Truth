b3e9637ec96c02a71714607135417e168291d011
Brandon Fulljames <bfullj@gmail.com>
2018-11-07 07:04:02 -0800

    Adding ads

---
+++ WGJ68Truth/Assets/Scripts/AdInitializer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Monetization;
+
+public class AdInitializer : MonoBehaviour {
+
+    public bool TestMode = true;
+
+#if UNITY_IOS
+    const string gameID = "2897106";
+#elif UNITY_ANDROID
+    const string gameID = "2897105";
+#endif
+
+    void Start () {
+        if (!Monetization.isInitialized && Monetization.isSupported)
+        {
+            Monetization.Initialize(gameID, TestMode);
+        }
+	}
+}
---
+++ WGJ68Truth/Assets/Scripts/AdPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Monetization;
+
+public class AdPlayer : MonoBehaviour {
+    public void Show()
+    {
+        ShowAdCallbacks options = new ShowAdCallbacks();
+
+        options.finishCallback = HandleResult;
+
+        ShowAdPlacementContent ad = Monetization.GetPlacementContent("rewardedVideo") as ShowAdPlacementContent;
+
+        ad.Show(options);
+    }
+
+    void HandleResult(ShowResult result) {
+        Debug.Log(result);
+    }
+}
