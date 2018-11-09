0084924864e5405fd4559672d6f3869b1784769c
Brandon Fulljames <bfullj@gmail.com>
2018-11-08 19:36:48 -0800

    Add zoom slider

---
+++ WGJ68Truth/Assets/ZoomableCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class ZoomableCamera : MonoBehaviour {
+    private CinemachineVirtualCamera camera;
+    private CinemachineFramingTransposer transposer;
+
+	// Use this for initialization
+	void Start () {
+        camera = GetComponent<CinemachineVirtualCamera>();
+        transposer = camera.GetCinemachineComponent<CinemachineFramingTransposer>();
+
+        if (transposer == null)
+        {
+            Debug.LogError("Could not find transposer on camera object");
+        }
+	}
+
+    public void SetCameraDistance(float distance)
+    {
+        transposer.m_CameraDistance = distance; 
+    }
+}
