7511d87c65d164d741c0cc06e4b6e74720b2c5e5
Brandon Fulljames <bfullj@gmail.com>
2018-11-09 06:29:07 -0800

    Add loading bar

--- WGJ68Truth/Assets/Scripts/MenuActions.cs
+++ WGJ68Truth/Assets/Scripts/MenuActions.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Analytics;
 
 [RequireComponent(typeof(AdPlayer))]
 public class MenuActions : MonoBehaviour {
     AdPlayer adPlayer;
+
+    public Slider LoadingBar;
+    public GameObject MenuContainer;
+    public GameObject LoadingBarContainer;
 
     public void Start()
     {
@@ -38,7 +43,35 @@ public class MenuActions : MonoBehaviour {
 
         AnalyticsEvent.LevelStart(size);
 
-        SceneManager.LoadScene("Play");
+        if (LoadingBar != null)
+        {
+            LoadingBarContainer.SetActive(true);
+            MenuContainer.SetActive(false);
+
+            StartCoroutine(StartAsync(size));
+        }
+        else
+        {
+            SceneManager.LoadScene("Play");
+        }
+    }
+
+    IEnumerator StartAsync(int size)
+    {
+        var progress = SceneManager.LoadSceneAsync("Play");
+
+        LoadingBar.minValue = 0;
+        LoadingBar.maxValue = 0.9f; // Scene will be swapped in after 0.9, so measure up until there
+        LoadingBar.value = 0;
+
+        while (!progress.isDone)
+        {
+            LoadingBar.value = progress.progress;
+
+            Debug.Log(progress.progress);
+
+            yield return null;
+        }
     }
 
     public void QuitGame()
